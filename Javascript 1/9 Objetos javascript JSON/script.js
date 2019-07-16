3b2b5949eeb78ad059cef5d8c4cf62477bbd5134
Rodolfo Luiz Soares dos Santos <rodolfo.luiz@rjo01.local>
2019-07-16 19:49:44 -0300

    trabalhando com json, criando um arrayde estudantes e iterando sobre o objeto

---
+++ Javascript 1/9 Objetos javascript JSON/script.js
@@ -0,0 +1,22 @@
+
+var  output =  document.getElementById("output");
+var a = "Minha saída<br>";
+
+var meuObjeto = {
+    "estudantes" : [
+        {"primeiroNome" : "Rodolfo", "empresa" : "TOTVS"},
+        {"primeiroNome" : "Luciana", "empresa" : "unknown"},
+        {"primeiroNome" : "Roberta", "empresa" : "unknown"},
+        {"primeiroNome" : "Monica",  "empresa"  : "TOOLS"},
+        {"primeiroNome" : "Rosana",  "empresa"  : "unknown"}
+    ]
+       
+};
+
+document.getElementById("btn1").onclick = function() {
+
+
+for(i=0; i < meuObjeto.estudantes.length; i++ ) {
+output.innerHTML += meuObjeto.estudantes[i].primeiroNome + " " + meuObjeto.estudantes[i].empresa + "<br>";
+}
+}
